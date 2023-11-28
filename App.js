fd4cf860abec765e323472d5e83c02c664d1d1a1
Profe Pablo <pablo-macia@live.com.ar>
2023-11-27 21:28:35 -0300

    Manejo de estados y eventos, TextInput y Button Add

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import {useState} from 'react'
 
 export default function App() {
+  const [textItem, setTextItem] = useState('')
+  const [itemList, setItemList] = useState([])
+
+  const onChangeTextHandler = (text) => {
+    setTextItem(text)
+  }
+
+  const addItemToList = () => {
+    setItemList(prevState => [...prevState,{id: Math.random().toString(),value:textItem}])
+    console.log(itemList)
+    setTextItem('')
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Ingresar tarea" />
-        <Button title="Add" color="#841584" />
+        <TextInput 
+          style={styles.textInput} 
+          placeholder="Ingresar tarea" 
+          onChangeText={onChangeTextHandler}
+          value={textItem}
+        />
+        <Button 
+          title="Add" 
+          color="#841584"
+          onPress={addItemToList} 
+        />
       </View>
       <View></View>
     </View>
